cb18be00c01dfba334d6d3bd9e748a61006acac4
itzel <itzeljimenez.do@gmail.com>
2017-05-29 02:39:29 -0500

    botones de enviar enlazados para funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,35 @@
     selectMonths: true, 
     selectYears: 15 
   });
+
+  function initMap(){
+    var mapaContenedor = document.getElementById("map");
+  	var coordenadasCDMX= {lat:19.2542, lng:-99.0740};
+ 
+  	var mapa = new google.maps.Map(mapaContenedor,{
+       zoom:8,
+        center: coordenadasCDMX
+      });
+    var marcador = new google.maps.Marker({
+      position: {lat:19.2542, lng:-99.0740},
+      map:mapa,
+      title: "CDMX",
+      draggable: true,
+ 	});
+ 	 var ubication = document.getElementById("ubication");
+ 	 new google.maps.places.Autocomplete(ubication);
+ };
+
+ function prueba(){
+ 	alert("Los botones están bien enlazados");
+ }
+
+ var sendMessage = document.getElementById('sendMessage');
+ var sendImage = document.getElementById('sendImage');
+ var sendEvent = document.getElementById('sendEvent');
+ var sendMedia = document.getElementById('sendMedia'); 	
+
+ sendMessage.addEventListener("click", prueba);
+ sendImage.addEventListener("click", prueba);
+ sendEvent.addEventListener("click", prueba);
+ sendMedia.addEventListener("click", prueba);
